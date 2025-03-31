ad8a2b898a88c47eed68e0e908960833cbb5550d
jschwartzman7 <jfs1005@gmail.com>
2025-03-31 13:03:04 -0500

    work

--- skateVideos.js
+++ skateVideos.js
@@ -1,21 +1,13 @@
-let skateVideosContainer = document.getElementById("skateVideosContainer");
+let skateVideosContainer = document.querySelector(".videoContainer");
 for (let skateVideo of skateVideosContainer.children) {
     skateVideo.addEventListener("click", function() {
-        let pauseVideos = false;
         if(skateVideo.paused) {
             skateVideo.play();
             skateVideo.width = 800;
-            pauseVideos = true;
         }
         else {
             skateVideo.pause();
             skateVideo.width = 300;
-        }
-        for (let skateVideo2 of skateVideosContainer.children) {
-            if (skateVideo2 != skateVideo) {
-                skateVideo2.pause();
-                skateVideo2.width = 300;
-            }
         }
     });
 }
